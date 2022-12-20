b9093eeda4f0119f4bd3599866b893c249c4b72a
RavoPL <ravosilesia88@protonmail.com>
2022-12-20 18:12:02 +0000

    Add quiz questions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,94 @@ const submitButton = document.getElementById('submit');
 
 /* -- basic functions of the quiz -- */
 
+/* logic for building the quiz structure */
+function buildQuiz(){}
+
+/* logic for displaying quiz results */
+function showResults(){}
+
+/* displays the quiz structure */
+buildQuiz();
+
+/* displays results when 'submit' button is pressed */
+submitButton.addEventListener('click', showResults);
+
+/* -- a list of questions to display for the Foundations of Deen section -- */
+
+const questionsDeen = [
+  {
+    question: "What is the month of fasting called?",
+    answers: {
+      a: "Tawheed",
+      b: "Kaaba",
+      c: "Ramadan"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What are the fundamental and obligatory acts of worship called?",
+    answers: {
+      a: "Seven Rites",
+      b: "Holy Trinity",
+      c: "Five Pillars",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "How many names does Allah SWT have?",
+    answers: {
+      a: "1",
+      b: "99",
+      c: "33",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "How many times a day are Muslims required to pray?",
+    answers: {
+      a: "Five times",
+      b: "Twice",
+      c: "Three times",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "What are the followers of Islam called?",
+    answers: {
+      a: "Sunnis",
+      b: "Muslims",
+      c: "Shias",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "How many surahs does the Qur'an have?",
+    answers: {
+      a: "86",
+      b: "122",
+      c: "114",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "What is the name of the first woman to become a Muslim?",
+    answers: {
+      a: "Khadijah",
+      b: "Fatima",
+      c: "Amina",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "What is the qiblah?",
+    answers: {
+      a: "Ritual ablution",
+      b: "Direction of prayer",
+      c: "Remembrance of Allah SWT",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: ""
+  }
+]
