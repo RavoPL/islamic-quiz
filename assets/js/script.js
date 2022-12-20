fe8fec095785665877f0bb462290f406189dabde
RavoPL <ravosilesia88@protonmail.com>
2022-12-20 23:09:17 +0000

    Add ten more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,92 +210,92 @@ const questionsHistory = [
     correctAnswer: "a"
   },
   {
-    question: "Which Islamic movement was adopted by the House of Saud in 1744?",
+    question: "Who introduced the Islamic calendar?",
     answers: {
-      a: "Mahdavia",
-      b: "Salafism",
-      c: "Wahhabism",
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "What is the Zulfiqar?",
-    answers: {
-      a: "A knife",
-      b: "A spear",
-      c: "A sword",
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Who founded the Mawlawiyah Sufi order?",
-    answers: {
-      a: "Shams Tabrizi",
-      b: "Ibn Arabi",
-      c: "Rumi",
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Who founded the Hanbali school?",
-    answers: {
-      a: "Ahmad ibn Hanbal",
-      b: "Ibn Taymiyyah",
-      c: "Abu Hanifa",
-    },
-    correctAnswer: "a"
-  },
-  {
-    question: "What were the first four Caliphs collectively called?",
-    answers: {
-      a: "Rashidun",
-      b: "Shah",
-      c: "Shaykh al-Islam",
-    },
-    correctAnswer: "a"
-  },
-  {
-    question: "Who constructed the Great Mosque of Damascus?",
-    answers: {
-      a: "Maulana Sher Ali",
-      b: "al-Walid I",
-      c: "Umar II",
+      a: "Uthman ibn Affan",
+      b: "Umar ibn al-Khattab",
+      c: "Hasan ibn Ali",
     },
     correctAnswer: "b"
   },
   {
-    question: "Which battle was led by the Prophet SAW during Ramadan?",
+    question: "Which of these battles did Khalid ibn al-Walid fight AGAINST Muslims?",
     answers: {
-      a: "Battle of Badr",
-      b: "Battle of Uhud",
-      c: "Battle of the Trench",
-    },
-    correctAnswer: "a"
-  },
-  {
-    question: "What was the purpose of the Prophet SAW's hijrah to Medina?",
-    answers: {
-      a: "To escape plague",
-      b: "To spread Islam",
-      c: "To escape persecution",
+      a: "Battle of Hunayn",
+      b: "Battle of Walaja",
+      c: "Battle of Uhud",
     },
     correctAnswer: "c"
   },
   {
-    question: "Who succeeded the Prophet SAW after his death?",
+    question: "Which Muslim commander was victorious at the Battle of Hattin?",
     answers: {
-      a: "Abu Hurairah",
-      b: "Abu Bakr",
-      c: "Khalid ibn al-Walid",
+      a: "Salahudin al-Ayubi",
+      b: "Timur",
+      c: "Ahmad ibn Hanbal",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which enemy did the mujahideen fight in Afghanistan in the Cold War?",
+    answers: {
+      a: "Pakistan",
+      b: "USA",
+      c: "Soviet Union",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which country invaded Iraq in 2003?",
+    answers: {
+      a: "Iran",
+      b: "USA",
+      c: "Syria",
     },
     correctAnswer: "b"
   },
   {
-    question: "What does the term Islam mean?",
+    question: "Which Islamic empire ruled India and Afghanistan for over 300 years?",
     answers: {
-      a: "Submission",
-      b: "Peace",
-      c: "Holy",
+      a: "Safavid Empire",
+      b: "Ottoman Empire",
+      c: "Mughal Empire",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Ibn Sina was a Persian polymath. Which modern day country is his birthplace in?",
+    answers: {
+      a: "Uzbekistan",
+      b: "Iran",
+      c: "Tajikistan",
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which Abbasid ruler established the Bayt al-Hikmah in Iraq?",
+    answers: {
+      a: "Al Mutassim",
+      b: "Al Hakim bi-Amrallah",
+      c: "Harun al-Rashid",
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Where was the philosopher ibn Rushd born?",
+    answers: {
+      a: "Baghdad, Iraq",
+      b: "Cordoba, Spain",
+      c: "Cairo, Egypt",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What is al-Khwarizmi best known for?",
+    answers: {
+      a: "The development of algebra",
+      b: "Codifying the Arabic language",
+      c: "Discovering the circulatory system",
     },
     correctAnswer: "a"
   }
